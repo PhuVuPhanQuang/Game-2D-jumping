eae366a6182cabe5ef2ccddb5881a507f72f3697
Vũ Phan Quang Phú <85773628+RyderMartinez@users.noreply.github.com>
2023-11-12 17:17:06 +0700

    first commit
    first commit

---
+++ Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLife : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    [SerializeField] private AudioSource deathSoundEffect;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    /// <summary>
+    /// tac dung ham khi player dap trap thi chet
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Trap"))
+        {
+            Die();
+        }
+    }
+
+    /// <summary>
+    /// Ham xu ly death
+    /// </summary>
+    private void Die()
+    {
+        deathSoundEffect.Play();
+        rb.bodyType = RigidbodyType2D.Static;
+        anim.SetTrigger("death");
+    }
+
+    /// <summary>
+    /// Ham reset level khi da chet
+    /// </summary>
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
