8b64c6d27e7fc46b9c5cadbfee8f8996428e20ce
Vũ Phan Quang Phú <85773628+RyderMartinez@users.noreply.github.com>
2023-11-12 23:47:28 +0700

    Update Quit()
    Update Quit()

--- Assets/Scripts/EndMenu.cs
+++ Assets/Scripts/EndMenu.cs
@@ -1,11 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EndMenu : MonoBehaviour
 {
-    public void Quit()
+    public GameObject quit;
+    private Button quitButton;
+
+    void Start()
     {
-        Application.Quit();
+        quitButton = quit.GetComponent<Button>();
+    }
+
+    /*public void Quit()
+    {
+         Application.Quit();
+    }*/
+    void Update()
+    {
+        //if ((Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl)) && Input.GetKeyDown(KeyCode.Q))
+        //{
+        //    quitButton.onClick.Invoke();
+        //}
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            QuitApp();
+        }
+    }
+
+    public void QuitApp()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+            Application.Quit();
     }
 }
